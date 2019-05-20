ee4e058f286cddba7f833fbfddf94f59267d01ba
Karyn Vo <vkaryn@gmail.com>
2019-05-19 23:22:12 -0700

    added contact footer to small screen

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,4 +2,8 @@ jQuery(document).ready(function ($) {
   $('.navbar-close').on('click',function() {
     $('#navbarSupportedContent').removeClass('show');
   });
+
+  $('.contact-footer-info').on('click',function() {
+    $('.contact-footer').toggleClass('bar-active');
+  });
 });
