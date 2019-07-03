9f05fce82e367cd6c36e33b248a2189cfc4a4592
Karyn Vo <kbvo@uw.edu>
2019-07-02 23:40:16 -0700

    resized contact bar

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,46 @@
 jQuery(document).ready(function ($) {
+
+  var stickyThreshold = document.getElementById("stick").offsetTop - document.getElementById("banner").offsetTop;
+
+  // When the user scrolls the page, execute myFunction 
+  window.onscroll = function() {myFunction(stickyThreshold)};
+
+  // Get the navbar
+  var navbar = document.getElementById("stick");
+  var headerContact = document.getElementById("header-contact");
+
+  // Add the sticky class to the navbar when you reach  its scroll position. Remove "sticky" when you leave  the scroll position
+  function myFunction() {
+    if(window.pageYOffset > 0){
+      document.getElementById("icon-logo-img").classList.add("skinny")
+      headerContact.classList.add("short");
+    }else {
+      document.getElementById("icon-logo-img").classList.remove("skinny")
+      headerContact.classList.remove("short");
+    }
+
+    // Get the offset position of the navbar
+    var sticky = navbar.offsetTop;
+    var bannerOffset = document.getElementById("banner").offsetTop;
+    var nav = document.getElementById("banner").offsetTop;
+    var navOffset = headerContact.offsetHeight;
+    var turnToSticky = sticky - bannerOffset;
+
+    console.log("window.pageYOffset: " + window.pageYOffset);
+    console.log("turnToSticky: " + turnToSticky);
+    console.log("navOffset: " + navOffset);
+    console.log("sticky: " + sticky);
+    console.log("bannerOffset: " + bannerOffset);
+    if (window.pageYOffset > stickyThreshold){
+      navbar.classList.add("sticky");
+      navbar.style.top = navOffset + "px";
+    } 
+    else{
+      navbar.classList.remove("sticky");
+      // navbar.style.top = "auto";
+    } 
+  }
+
   var $sub = $("#_subject");
 	$("#email").on("input", function() {
 		$sub.val("Request Appointment (" + $(this).val() + ")");
