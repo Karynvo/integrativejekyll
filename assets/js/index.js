8e8300881372c38ed7b81a11594a95c006466af6
Karyn Vo <kbvo@uw.edu>
2019-08-07 20:08:59 -0700

    readded banner for smaller width screen

--- assets/js/index.js
+++ assets/js/index.js
@@ -60,26 +60,52 @@ function listen(evnt, elem, func) {
 listen("error", window, function(){ $("#cover").remove(); });
 
 listen("load", window, function () {
+
+  var stickyThreshold = document.getElementById("stick").offsetTop + document.getElementById("banner").offsetTop;
+  
+  var initializeThreshold = function () {
+    console.log("height: " + document.getElementById("banner").offsetHeight )
+    console.log("top: " + document.getElementById("banner").offsetTop )
+    // if (window.innerWidth < 1199)
+    stickyThreshold = document.getElementById("banner").offsetHeight + document.getElementById("banner").offsetTop;
+  //   else
+  //   stickyThreshold = document.getElementById("stick").offsetTop - document.getElementById("banner").offsetTop;
+  }
+  
+  initializeThreshold();
+
   // Get the navbar
   var navbar = document.getElementById("stick");
   var headerContact = document.getElementById("header-contact");
-  
+
   // Add the sticky class to the navbar when you reach  its scroll position. Remove "sticky" when you leave  the scroll position
   var myFunction = function () {
+    if(window.innerWidth >= 1199){
+      document.getElementById("banner").classList.add("hide");
     if (window.pageYOffset > 0) {
       document.getElementById("icon-logo").classList.remove("hide");
-      document.getElementById("banner").classList.add("hide");
+      document.getElementById("banner-logo").classList.add("hide");
       headerContact.classList.add("short");
       navbar.style.top = headerContact.offsetHeight + "px";
       navbar.classList.add("sticky");
     } else {
       document.getElementById("icon-logo").classList.add("hide");
-      document.getElementById("banner").classList.remove("hide");
+      document.getElementById("banner-logo").classList.remove("hide");
       headerContact.classList.remove("short");
       navbar.classList.remove("sticky");
     }
+  }else{
+    document.getElementById("banner").classList.remove("hide");
     // add padding to body for "integrative dentistry" banner
-    document.body.style.paddingTop = document.getElementById("header-contact").offsetHeight + "px";
+    if (window.pageYOffset > stickyThreshold) {
+      navbar.style.top = headerContact.offsetHeight + "px";
+      navbar.classList.add("sticky");
+    }
+    else {
+      navbar.classList.remove("sticky");
+    }
+  }
+  document.body.style.paddingTop = document.getElementById("header-contact").offsetHeight + "px";
   };
   
   $("#cover").remove();
@@ -87,16 +113,6 @@ listen("load", window, function () {
   
   listen("scroll", window, function () { myFunction(); });
   listen("resize", window, function () { myFunction(); });
+  listen("resize", window, function () { initializeThreshold(); });
 
-  var newPhoto = true;
-  $('#togglePhoto').click(function(){
-    if(newPhoto){
-      $('#jumbotron').css('background-image', 'url(assets/images/Banff1.jpg)');
-      $('#jumbotron-text-wrapper').removeClass('align-items-center');
-    }else{
-      $('#jumbotron').css('background-image', 'url(assets/images/HomepagePicCrop.jpg)');
-      $('#jumbotron-text-wrapper').addClass('align-items-center');
-    }
-    newPhoto = !newPhoto;
-  })
 });
